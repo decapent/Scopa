8c4a7d9e030336d72208e70c8df1914a13c5bfc2
Patrick Lavallee <patrick.lavallee@gsoft.com>
2015-12-13 23:56:43 -0500

    Got a working setup of an indexed external table. The idea was to have partition names that matched the HDFS folder names in lower case... i bow before the MSCK repair TABLE command.

--- Source/Sporacid.Scopa.Scripts/database/Create-Tables.sql
+++ Source/Sporacid.Scopa.Scripts/database/Create-Tables.sql
@@ -1,26 +1,15 @@
--------------------------------------------------------------------------
--- SP2013 Logs
--------------------------------------------------------------------------
-CREATE EXTERNAL TABLE IF NOT EXISTS default.SP2013Logs(logdate date,
-       process string,
-       threadId string,
-       area string,
-       category string,
-       eventId string,
-       severity string,
-       message string,
-       correlation string)
-COMMENT 'Log files from SharePoint 2013'
-ROW FORMAT DELIMITED
-        FIELDS TERMINATED BY '\t'
-        COLLECTION ITEMS TERMINATED BY '\002'
-        MAP KEYS TERMINATED BY '\003'
-STORED AS TEXTFILE
-LOCATION '/hive/warehouse/SP2013Logs'
+--------------------------------------------------------------------
+-- Environment variables
+--------------------------------------------------------------------
+SET hive.exec.dynamic.partition=true;
+SET hive.exec.dynamic.partition.mode = nonstrict; 
+SET hive.exec.max.dynamic.partitions=50000; 
+SET hive.exec.max.dynamic.partitions.pernode=20000;
 
 --------------------------------------------------------------------
--- Indexed SP2013 Logs
+-- Indexed SP2013 Logs External Table
 --------------------------------------------------------------------
+DROP TABLE default.SP2013_Logs;
 CREATE EXTERNAL TABLE IF NOT EXISTS default.SP2013_Logs(
        datedesc date,
        process string,
@@ -32,34 +21,14 @@ CREATE EXTERNAL TABLE IF NOT EXISTS default.SP2013_Logs(
        message string,
        correlation string)
 COMMENT 'Aggregates SharePoint 2013 Log files'
-PARTITIONED BY (MachineName string COMMENT 'Machine name index',
-       LogDate string COMMENT 'Log Date entry index')
+PARTITIONED BY (hostname string COMMENT 'host name index',
+                logdate string COMMENT 'log date entry index')
 ROW FORMAT DELIMITED
         FIELDS TERMINATED BY '\t'
         COLLECTION ITEMS TERMINATED BY '\002'
         MAP KEYS TERMINATED BY '\003'
 STORED AS TEXTFILE
-LOCATION '/hive/warehouse/SP2013_Logs'
+LOCATION '/hive/warehouse/SP2013_Logs';
 
--- Machine name partition
-ALTER TABLE SP2013_Logs ADD PARTITION(MachineName='VMSPPLAV');
-ALTER TABLE SP2013_Logs ADD PARTITION(MachineName='DESJ-CONTENT');
-
--- Log Date partition
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151125');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151126');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151127');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151128');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151129');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151130');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151201');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151202');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151203');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151204');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151205');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151206');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151207');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151208');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151209');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151210');
-ALTER TABLE SP2013_Logs ADD PARTITION(LogDate='20151211');
+-- Scans for missing partitions and adds them to the hive metastore
+msck repair TABLE default.SP2013_Logs;
