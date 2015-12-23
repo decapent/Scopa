5ab8d84dadd12181b5c8478d8c192faafab00a90
Decapent <lavalleep@gmail.com>
2015-12-22 20:49:58 -0500

    Indexed Table architecture schema under HDFS

--- Source/Sporacid.Scopa.Scripts/Deploy-HadoopData.ps1
+++ Source/Sporacid.Scopa.Scripts/Deploy-HadoopData.ps1
@@ -10,14 +10,26 @@ Param (
 	
 	[Parameter(Mandatory=$True)]
 	[string]$HiveTableName
+	
+	[switch]$FetchRemote
 ) 
 
-if(Test-Path($DataSource)) {
+if(Test-Path($RawDataSource)) {
+	
+	$patterns = "[[SP2013_LOGFILE_REGEX_PATTERNS]]"
+
+	if ($FetchRemote.IsPresent) {
+		# Fetching logs remotely in target hosts... Incremental switch is used to fetch logs from a certain point in time.
+		# That point is saved in the config.xml file.
+		Write-Host "Looking on remote hosts for latests log files."
+		.\Fetch-RemoteLogs.ps1 -DumpFolder $RawDataSource -Incremental -RegexPatterns 
+	}
+	
 	# Creating Hive-ready folder structure and moving data to proper index.
 	.\Create-FolderArchitecture.ps1 -Datasource $RawDatasource -Destination $HiveTableRepository -HiveTableName $HiveTableName
 	
 	# Copying newly created architecture to Azure Blob Container and create external tables.
-	.\Upload-HiveTables.ps1 -Datasource $HiveTableRepository -StorageAccountName "nevermore" -StorageContainerName "abbath"
+	.\Upload-HiveTables.ps1 -Datasource $HiveTableRepository -StorageAccountName "[[AZURE_STORAGE_ACCOUNT_NAME]]" -StorageContainerName "[[AZURE_STORAGE_CONTAINER_NAME]]"
 } else {
 	Write-Host "Please submit a valid datasource path." -ForeGroundColor Yellow
 }
--- Source/Sporacid.Scopa.Scripts/Fetch-RemoteLogs.ps1
+++ Source/Sporacid.Scopa.Scripts/Fetch-RemoteLogs.ps1
@@ -1,8 +1,8 @@
 Param (
 	[string]$DumpFolder="\\VMSPPLAV\raw",
 	
-	[switch]$Incremental=$true,
-	[switch]$KeepStaging=$true,
+	[switch]$Incremental,
+	[switch]$KeepStaging,
 	
 	[Parameter(Mandatory=$true)]
 	[string[]]$RegexPatterns
@@ -12,15 +12,15 @@ Param (
 [Reflection.Assembly]::LoadWithPartialName( "System.IO.Compression.FileSystem" )
 
 # Array de client remote
-$remotePaths = @("\\VMSPHIL\C$\Logs", "\\VMSPPLAV\C$\Logs")
+$remotePaths = @("\\VMSPPHIL\C$\Logs", "\\VMSPPLAV\C$\Logs")
 
 $remotePaths | ForEach-Object {
 
 	Write-Host "Connecting to [$_]... " -NoNewLine
 	if (Test-Path($_)) {
 		Write-Host " success!" -ForeGroundColor Green
-				
-		$uri = New-Object System.Uri($_)
+		$uri = New-Object System.Uri($_)		
+		
 		# Create a staging folder
 		$sp2013logPath = $_ + "\SP2013"
 		$stagingFolderPath + "\staging"
@@ -32,7 +32,7 @@ $remotePaths | ForEach-Object {
 			# TODO: Write a function that fetch the config (config.xml maybe... not sure yet)
 			$lastIncrementalDate = "12/15/2015"
 			Write-Host "Incremental parameter found, files will be loaded from $lastIncrementalDate up to this day!" -ForeGroundColor Yellow
-			$files = $files | Where-Object {$_.CreationTime -ge $lastIncrementalDate}
+			$files = $files | Where-Object {$_.LastWriteDate -ge $lastIncrementalDate}
 		}
 		
 		# Now match the regex pattern
@@ -46,9 +46,9 @@ $remotePaths | ForEach-Object {
 			}
 		}
 
-		# Zip staging folder following [HOSTNAME_LOGTYPE_TIMESTAMP]		
-		$zipDestination = [string]::Format("\{0}_{1}_{2}.zip", $uri.Host, "SP2013", (Get-Date -Format "yyyyMMdd_hhmmss"))
+		# Zip staging folder following [HOSTNAME_LOGTYPE_TIMESTAMP]	nomenclature.
 		$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+		$zipDestination = [string]::Format("\{0}_{1}_{2}.zip", $uri.Host, "SP2013", (Get-Date -Format "yyyyMMdd_hhmmss"))
 		[System.IO.Compression.ZipFile]::CreateFromDirectory($stagingFolderPath, $zipDestination, $compressionLevel, $false)
 			
 		if (-not($KeepStaging.IsPresent)) {
