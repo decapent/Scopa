3a2c6c03ccfeefb0297fb0e67910ffc7e6ee6d98
Decapent <lavalleep@gmail.com>
2016-02-27 12:00:51 -0500

    Error handling added to Main upload logic

--- Sporacid.Scopa.Scripts/Client/Upload-LogsToServer.ps1
+++ Sporacid.Scopa.Scripts/Client/Upload-LogsToServer.ps1
@@ -15,37 +15,51 @@ $lastScopaDate = $xml.Configuration.LastScopaDate
 $logs = $xml.Configuration.Logs
 $remotehost = $xml.Configuration.RemoteHost
 
-foreach($log in $logs) {	
-	# Create a staging folder for file transfer and ultimately zip purposes
-	# following [HOSTNAME_LOGTYPE_TIMESTAMP] nomenclature.
-	$stagingFolder = [string]::Format("{0}_{1}_{2}", $hostname, $log.Type, $timestamp)
-	New-Item -Type Directory -Path $stagingFolder -Force
+Try 
+{
+	foreach($log in $logs) 
+	{	
+		# Create a staging folder for file transfer and ultimately zip purposes
+		# following [HOSTNAME_LOGTYPE_TIMESTAMP] nomenclature.
+		$stagingFolder = [string]::Format("{0}_{1}_{2}", $hostname, $log.Type, $timestamp)
+		New-Item -Type Directory -Path $stagingFolder -Force
 	
-	# Get the files and if IsIncremental, filter a first shot on the last write date.
-	$files = Get-ChildItem -Recurse -File -Path $log.LocalPath
-	if ($IsIncremental.IsPresent) {
-		Write-Host "Incremental detected! File will be loaded from [$lastScopaDate] to this day!"
-		$files = $files | Where-Object {$_.LastWriteDate -ge $lastScopaDate}
-	}
-	
-	# Now match the regex pattern on the file name
-	$files | ForEach-Object {
-		if($_.Name -match $log.FileNamePattern) {
-			Write-Host "Moving [$_] to [$stagingFolder]"
-			Copy-Item -Path $_ -Destination $stagingFolder
+		# Get the files and if IsIncremental, filter a first shot on the last write date.
+		$files = Get-ChildItem -Recurse -File -Path $log.LocalPath
+		if ($IsIncremental.IsPresent) 
+		{
+			Write-Host "Incremental parameter detected! File will be loaded from [$lastScopaDate] up to this day!"
+			$files = $files | Where-Object {$_.LastWriteDate.ToString() -ge $lastScopaDate}
 		}
-	}
+	
+		# Now match the regex pattern on the file name
+		$files | ForEach-Object 
+		{
+			if($_.Name -match $log.FileNamePattern) 
+			{
+				Write-Host "Moving [$_] to [$stagingFolder]"
+				Copy-Item -Path $_ -Destination $stagingFolder
+			}
+		}
 
-	$zipDestination = [string]::Format(".\{0}.zip", $stagingFolder)
-	$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
-	[System.IO.Compression.ZipFile]::CreateFromDirectory($stagingFolder, $zipDestination, $compressionLevel, $false)
+		$zipDestination = [string]::Format(".\{0}.zip", $stagingFolder)
+		$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+		[System.IO.Compression.ZipFile]::CreateFromDirectory($stagingFolder, $zipDestination, $compressionLevel, $false)
 		
-	# Move Zip to Destination on remote host
-	Move-Item -Path $zipDestination -Destination $remoteHost.Destination
+		# Move Zip to Destination on remote host
+		Move-Item -Path $zipDestination -Destination $remoteHost.Destination
 
-	# Remove staging folder
-	Remove-Item -Path $stagingFolderPath -Recurse -Force
-
+		# Remove staging folder
+		Remove-Item -Path $stagingFolderPath -Recurse -Force
+	}
+}
+Catch
+{
+	$errorMessage = $_.Exception.Message
+	Write-Host "Something went wong... [$errorMessage]"
+}
+Finally
+{
 	# Finally, update the last Scopa date for next run
 	$xml.Configuration.LastScopaDate = (Get-Date).ToString()
 	$configFilePath = Resolve-Path ".\config.xml"
