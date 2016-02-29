304d60d5968555bdc6ebf05b5660fab2252495dd
Patrick Lavallee <patrick.lavallee@gsoft.com>
2016-02-29 00:42:44 -0500

    Reworked the messages that was sent back to user. Oh! and don't miss the fancy header.

--- Sporacid.Scopa.Scripts/Client/Upload-LogsToServer.ps1
+++ Sporacid.Scopa.Scripts/Client/Upload-LogsToServer.ps1
@@ -1,6 +1,6 @@
 <#
     .SYNOPSIS
-        Process a local log directory to be uploaded to a remote host
+        Process a local log directory to be uploaded to a remote host.
 
     .DESCRIPTION
         Process a local log directory, such as SharePoint 2013 or IIS to be uploaded to a remote host.
@@ -26,7 +26,7 @@ Param (
 	[switch]$KeepStaging
 )
 
-# Needed for file archiving. Ease of transfer ++
+# Needed for file archiving
 [Reflection.Assembly]::LoadWithPartialName( "System.IO.Compression.FileSystem" )
 
 # Archive infos
@@ -42,67 +42,74 @@ $logs = $xml.Configuration.Logs
 
 Try 
 {
+	# Processing each type of log files
 	foreach($log in $logs.Log) 
-	{	
-		# Create a staging folder for file transfer and ultimately zip purposes
-		# following [HOSTNAME_LOGTYPE_TIMESTAMP] nomenclature.
-		$stagingFolderPath = [string]::Format("{0}\{1}_{2}_{3}", $log.LocalPath, $hostname, $log.Type, $timestamp)
-		Write-Host "Creating staging folder [$stagingFolderPath]" -ForegroundColor Green
-		New-Item -Type Directory -Path $stagingFolderPath -Force
-	
-		# Get the files and if IsIncremental, filter a first shot on the file's last write date.
+	{		
+		# Print some fancy header
+		$header = [string]::Format("Processing {0} logs @ {1}", $log.Type, $log.LocalPath)
+		Write-Host "-------------------------------------------------" -ForegroundColor Cyan
+		Write-Host $header -ForegroundColor Cyan
+		Write-Host "-------------------------------------------------" -ForegroundColor Cyan
+
+		# Get the files and if Incremental, filter a first shot on the file's last write date.
 		$files = Get-ChildItem -Recurse -File -Path $log.LocalPath
 		if ($Incremental.IsPresent) 
 		{
-			Write-Host "Incremental parameter detected! File will be loaded from [$lastScopaDate] up to this day!" -ForegroundColor Yellow
+			Write-Host "WARNING: Incremental parameter detected! Files will be loaded from [$lastScopaDate] up to this day!" -ForegroundColor Yellow
 			$files = $files | Where-Object {$_.LastWriteTime -ge $lastScopaDate}
 		}
 	
-        # No need to create anything if there are no files to process
+        # No need to create anything if there are no files to process after date filtering
         if($files.Count -gt 0)
         {
-		    # Now match the regex pattern on the file name
+			# Create a staging folder following [HOSTNAME_LOGTYPE_TIMESTAMP] nomenclature.
+			$stagingFolderPath = [string]::Format("{0}\{1}_{2}_{3}", $log.LocalPath, $hostname, $log.Type, $timestamp)
+			Write-Host "Creating staging folder [$stagingFolderPath]" -ForegroundColor Yellow
+			New-Item -Type Directory -Path $stagingFolderPath -Force
+
+		    # Match the regex pattern on the file name
 		    foreach($file in $files)
 		    {
 				# For matching files, move them to staging folder
 			    if($file.Name -match $log.FileNamePattern) 
 			    {
-				    Write-Host "Moving [$file] to staging folder [$stagingFolderPath]"
+				    Write-Host "Moving [$file] to staging folder"
 				    Copy-Item -Path $file.FullName -Destination $stagingFolderPath
 			    }
 		    }
 
-		    # Prepare Zip archive with optimal compression rate... for what it's worth
-		    Write-Host "Preparing the archive from staging folder... " -ForegroundColor Green
+		    # Prepare the staging folder archive with optimal compression level...
+		    Write-Host "Preparing the staging folder archive with optimal compression level... " -ForegroundColor Yellow -NoNewline
 		    $archivePath = "$stagingFolderPath.zip"
 		    $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
 		    [System.IO.Compression.ZipFile]::CreateFromDirectory($stagingFolderPath, $archivePath, $compressionLevel, $false)
+			Write-Host "Complete!" -ForegroundColor Green
 		
 		    # Move Zip to Destination on remote host
-		    Write-Host "Moving [$archivePath] -> [$destination]... " -ForegroundColor Green
+		    Write-Host "Moving [$archivePath] to [$destination]"
 		    Move-Item -Path $archivePath -Destination $destination
 
-		    if(-not($KeepStaging.IsPresent))
-		    {
-			    # Remove staging folder
-			    Write-Host "Cleaning staging environment [$stagingFolderPath]" -ForegroundColor Yellow
-			    Remove-Item -Path $stagingFolderPath -Recurse -Force
-		    }
-		    else
-		    {
-			    Write-Host "WARNING: Staging environment is still on disk... cleanup will need to be done manually!!" -ForegroundColor Yellow
-		    }
+			# Remove staging folder if not specified
+            if(-not($KeepStaging.IsPresent))
+	        {
+		        Write-Host "Cleaning staging environment... " -ForegroundColor Yellow -NoNewline
+		        Remove-Item -Path $stagingFolderPath -Recurse -Force
+				Write-Host "Complete!" -ForegroundColor Green
+	        }
+	        else
+	        {
+		        Write-Host "WARNING: Staging environment is still on disk... cleanup will need to be done manually!!" -ForegroundColor Yellow
+	        }
         }
         else
         {
-            Write-Host "WARNING: No files to process using the INCREMENTAL switch..." -ForegroundColor Yellow
+            Write-Host "WARNING: No files to process! Either the log directory is EMPTY or the last INCREMENTAL date filters all the files..." -ForegroundColor Yellow
         }
 	}
 }
 Catch
 {
-	$errorMessage = $_.Exception.Message
-	Write-Host "Something went wong... [$errorMessage]" -ForegroundColor Red
+	Write-Host $_.Exception.Message -ForegroundColor Red
 }
 Finally
 {
