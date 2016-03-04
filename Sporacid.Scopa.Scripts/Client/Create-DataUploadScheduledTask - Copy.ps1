8905422e03e6efc6a798ddada74886628874946c
Decapent <lavalleep@gmail.com>
2016-03-03 21:56:28 -0500

    *Reworked the logic behind the Scheduled task registering... The script will be used as deployment entry point on client machines.
    *The Upload-LogsToServer.ps1 script will only update the last date in the config file on a successfull Scopa.

--- Sporacid.Scopa.Scripts/Client/Create-DataUploadScheduledTask - Copy.ps1
+++ Sporacid.Scopa.Scripts/Client/Create-DataUploadScheduledTask - Copy.ps1
@@ -1,13 +1,37 @@
-
+[CmdletBinding()]
 Param(
-	[string]$TaskName="Scopa",
-	[string]$TaskPath="\Sporacid"
+	[string]$TaskPath="[[DSP_SCOPACLIENT_TASKPATH]]"
 )
-# Use this to create a scheduled task that executes powershell on local computer
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -command "& {C:\Scopa\Upload-LogsToServer.ps1 -IsIncremental}"'
-$trigger =  New-ScheduledTaskTrigger -Daily -At 8pm
 
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $TaskName -TaskPath $TaskPath -Description "Runs a powershell script that bundles development log files daily."
-# Now to figure out a way to push that remotely to a bunch of machines.
-# An array of UNC paths in an xml config will contains all the config for the job.
+# Ensure deployment folder
+$deploymentPath = "[[DSP_SCOPACLIENT_DEPLOYMENTFOLDER]]"
+if(-not(Test-Path($deploymentPath)) 
+{
+	New-Item -Type Directory -Path $deploymentPath 
+}
 
+# Once the folder is ensured, copy upload script and config to the deployment folder
+$workingDir = Get-Location
+$filesToCopy = Get-ChildItem $workingDir -File -Recurse
+foreach($file in $filesToCopy)
+{
+	Copy-Item $file -Destination $deploymentPath -Force
+}
+
+# To finalize the deployment, the scheduled task in charge of gathering the log files is registered
+Try
+{
+	Write-Host "Registering Scopa Scheduled Task... " -NoNewLine
+	$taskScript = Join-Path $deploymentPath "Upload-LogsToServer.ps1 -Incremental"
+	$taskArgs = "-WindowStyle Hidden -NonInteractive -Executionpolicy unrestricted -File $taskScript"
+	$taskAction = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument $taskArgs
+	$taskTrigger =  New-ScheduledTaskTrigger -Daily -At 8pm
+
+	Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger -TaskName "Scopa Client - Incremental" -TaskPath $TaskPath -Description "Runs a powershell script that bundles development log files daily."
+	Write-Host "complete!" -ForeGroundColor Green
+} 
+Catch
+{
+	Write-Host "failed!" -ForeGroundColor Red
+	Write-Host $_.Exception.Message -ForeGroundColor Red
+}
