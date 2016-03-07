082d66c05d4f87364334edd20adac2c21efef359
Decapent <lavalleep@gmail.com>
2016-03-06 21:18:15 -0500

    @home on local laptop... Committing stuff before hopping on work Computer for better performance

---
+++ Scopa/Entities/WindowsLogArchive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sporacid.Scopa.Entities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class WindowsLogArchive : BaseLogArchive
+    {
+        /// <summary>
+        /// Create a new instance of a SharePoint2013LogArchive
+        /// </summary>
+        /// <param name="DataSourcePath">Path to repository of unprocesssed SP2013 log file</param>
+        public WindowsLogArchive(string DataSourcePath)
+            : base(LogTypes.Windows)
+        {
+            this.DataSourcePath = DataSourcePath;
+            this.ArchiveDate = DateTime.Now;
+        }
+    }
+}
